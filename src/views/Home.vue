2d872ca7922d4dcfb2afd7cc24eaa8a0740ee8a8
Hakan Ünlü <unluhak@outlook.com>
2021-08-24 22:27:51 +0200

    home component styled

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,19 +16,52 @@
         </div>
       </router-link>
     </div>
-    <form @submit.prevent="" class="search-box">
-      <input type="text" placeholder="Wonach suchst du?" />
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" placeholder="Wonach suchst du?" v-model="search" />
       <input type="submit" value="Search" />
     </form>
-    <div class="movies-list">MOVIES</div>
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="y">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { ref } from 'vue';
+import env from '@/env.js';
 export default {
-  name: 'Home',
+  setup() {
+    const search = ref('');
+    const movies = ref([]);
+
+    const SearchMovies = () => {
+      if (search.value != '') {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = '';
+          });
+      }
+    };
+    return {
+      search,
+      movies,
+      SearchMovies,
+    };
+  },
 };
 </script>
 
@@ -106,6 +139,60 @@ export default {
 
         &:active {
           background-color: #3b8070;
+        }
+      }
+    }
+  }
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: #fff;
+            left: 0px;
+            bottom: 16px;
+            text-transform: capitalize;
+          }
+        }
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          .y {
+            color: #aaa;
+            font-size: 14px;
+          }
+          h3 {
+            color: #fff;
+            font-weight: 600;
+            font-size: 18px;
+          }
         }
       }
     }
